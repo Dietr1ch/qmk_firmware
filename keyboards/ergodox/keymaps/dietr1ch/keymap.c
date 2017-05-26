63a2074cc8a479b85a4d01a3efb3590f538383b7
Dietrich Daroch <dietr1ch@acm.org>
2017-05-25 21:50:12 -0700

    Custom Keymap: Layer 0 easy changes

--- keyboards/ergodox/keymaps/dietr1ch/keymap.c
+++ keyboards/ergodox/keymaps/dietr1ch/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
 /* Keymap 0: Basic layer
  * ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |
+ * |   =    |   1  |   2  |   3  |   4  |   5  |   6  |
  * |--------+------+------+------+------+-------------|
  * | Del    |   Q  |   W  |   E  |   R  |   T  |  L1  |
  * |--------+------+------+------+------+------|      |
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |Grv/L1|  '"  |AltShf| Left | Right|
  *   `----------------------------------'
  */
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
+        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
         KC_DELT,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_BSPC,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
@@ -41,18 +41,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                                    | App  | LGui |
  *                                             ,------|------|------|
  *                                             |      |      | Home |
- *                                             |  SPC | BSPC |------|
+ *                                             | BSPC |  TAB |------|
  *                                             |      |      | End  |
  *                                             `--------------------'
  */
                                                      ALT_T(KC_APP),  KC_LGUI,
                                                                      KC_HOME,
-                                            KC_SPC,  KC_BSPC,        KC_END,
+                                            KC_BSPC, KC_TAB,         KC_END,
 /* Keymap 0: Right hand
  *                    ,--------------------------------------------------.
- *                    | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
+ *                    |   7  |   8  |   9  |   0  |   -  |   =  |   \    |
  *                    |------+------+------+------+------+------+--------|
- *                    |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ *                    |  L1  |   Y  |   U  |   I  |   O  |   P  |   [    |
  *                    |      |------+------+------+------+------+--------|
  *                    |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
  *                    | Meh  |------+------+------+------+------+--------|
@@ -62,8 +62,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                  `----------------------------------'
  */
 
-             KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
-             TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
+             KC_7,        KC_8,   KC_9,  KC_0,   KC_MINS,KC_EQL,           KC_BSLS,
+             TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_LBRC,
                           KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
              MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
                                   KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
@@ -72,13 +72,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *    | Alt  |Ctrl/Esc|
  *    |------+--------+------.
  *    | PgUp |        |      |
- *    |------|  Tab   |Enter |
+ *    |------|  Enter |  SPC |
  *    | PgDn |        |      |
  *    `----------------------'
  */
        KC_LALT,  CTL_T(KC_ESC),
        KC_PGUP,
-       KC_PGDN,  KC_TAB,    KC_ENT
+       KC_PGDN,  KC_ENT,    KC_SPC
     ),
 
 

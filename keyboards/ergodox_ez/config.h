bd91e9b2fe0d3462075cbb421f2fe84ef56475fd
Dietrich Daroch <dietr1ch@acm.org>
2018-04-14 11:50:11 -0700

    Halve hue change step

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -74,7 +74,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_DI_PIN D7
 #define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 15     // Number of LEDs
-#define RGBLIGHT_HUE_STEP 12
+#define RGBLIGHT_HUE_STEP 6
 #define RGBLIGHT_SAT_STEP 255
 #define RGBLIGHT_VAL_STEP 12
 

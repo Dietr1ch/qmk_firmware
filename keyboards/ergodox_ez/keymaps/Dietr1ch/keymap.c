589138130bc42da5c95e229d82714941a56dd4a1
Dietrich Daroch <dietr1ch@acm.org>
2018-04-14 11:58:42 -0700

    Tweak the game layer. Removed some dual-keys and added alt.

--- keyboards/ergodox_ez/keymaps/Dietr1ch/keymap.c
+++ keyboards/ergodox_ez/keymaps/Dietr1ch/keymap.c
@@ -162,11 +162,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |   TAB  |   Q  |   W  |   E  |   R  |   T  |   7  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   [    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  Ctrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |' / Cmd |
+ * |  Ctrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------|   8  |           | Meh  |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | LCTL |   Y  |   H  |   N  |  SPC |                                      | RAlt |   [  |   ]  |   \  | ~L1  |
+ *   | LCTL |   Y  |   H  |   N  |Alt/Sp|                                      | RAlt |   [  |   ]  |   \  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |  <-  |  ->  |       | Alt  |Ctrl/Esc|
@@ -182,7 +182,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,   KC_7,
         KC_LCTL,  KC_A,  KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,  KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,   KC_8,
-        KC_LCTL,  KC_Y,  KC_H,   KC_N,   KC_SPC,
+        KC_LCTL,  KC_Y,  KC_H,   KC_N,   LALT(KC_SPC),
 
                                                     KC_LEFT,  KC_RIGHT,
                                                               KC_UP,
@@ -190,9 +190,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         // right hand
              KC_7,        KC_8,  KC_9,   KC_0,   KC_MINS, KC_EQL,           KC_RBRC,
-             KC_TRNS,    KC_Y,  KC_U,   KC_I,   KC_O,    KC_P,             KC_LBRC,
-                          KC_H,  KC_J,   KC_K,   KC_L,    KC_SCLN,          GUI_T(KC_QUOT),
-             MEH_T(KC_NO),KC_N,  KC_M,   KC_COMM,KC_DOT,  CTL_T(KC_SLSH),   KC_RSFT,
+             KC_TRNS,     KC_Y,  KC_U,   KC_I,   KC_O,    KC_P,             KC_LBRC,
+                          KC_H,  KC_J,   KC_K,   KC_L,    KC_SCLN,          KC_QUOT,
+             MEH_T(KC_NO),KC_N,  KC_M,   KC_COMM,KC_DOT,  KC_SLSH,          KC_RSFT,
                                  KC_RALT,KC_LBRC,KC_RBRC, KC_BSLS,          KC_FN1,
 
        KC_LALT,  CTL_T(KC_ESC),
